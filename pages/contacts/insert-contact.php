601e57f5587d3c5c14119111e14d9d43e9a1aba6
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-11-25 19:11:13 -0300

    validation JS refactored

--- pages/contacts/insert-contact.php
+++ pages/contacts/insert-contact.php
@@ -1,6 +1,6 @@
 <header>
   <h3>
-    Insert Contact
+    Update Contact
   </h3>
 </header>
 <?php
@@ -8,13 +8,14 @@
   $email = mysqli_real_escape_string($connection, $_POST["email"]);
   $phone = mysqli_real_escape_string($connection, $_POST["phone"]);
 
-  $sql = "INSERT INTO contacts (
-  name,email,phone) VALUES(
-  '{$name}', '{$email}', '{$phone}'
-  )
+  $sql = "UPDATE contacts SET
+  name = {$name},
+  email = {$email},
+  phone = {$phone},
+  WHERE id = $id
   ";
 
   mysqli_query($connection, $sql) or die("Contact creation error" . mysqli_error($connection));
 
-  echo "Contact successfully created";
+  echo "Contact successfully updated";
 ?>
