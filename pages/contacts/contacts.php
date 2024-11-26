a106f783bc9faae7700a5f0146042dbafcf52152
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-11-25 23:53:21 -0300

    chore: add search input and incompleted pagination

--- pages/contacts/contacts.php
+++ pages/contacts/contacts.php
@@ -2,6 +2,11 @@
   <h3>Contacts!</h3>
 </header>
 <div>
+  <form action="index.php?menu=contacts" method="post">
+    <input type="text" name="txt_search" />
+    <input type="submit" value="Search" />
+    
+  </form>
   <button>
     <a href="index.php?menu=add-contact">New contact.</a>
   </button>
@@ -20,7 +25,17 @@
   </thead>
   <tbody>
     <?php
-      $sql = "SELECT * FROM contacts";
+
+      $quantityOfContactsPerPage = 10;
+      $page = (isset($_GET["page"]))? (int)$_GET["page"] : "";
+
+      $start = ($quantityOfContactsPerPage * $page) - $quantityOfContactsPerPage;
+
+    $txt_search = (isset($_POST["txt_search"]))? $_POST["txt_search"] : "";
+      $sql = "SELECT * FROM contacts WHERE id='{$txt_search}' or name LIKE '%{$txt_search}%'
+      ORDER BY name ASC
+      LIMIT $start, $quantityOfContactsPerPage;
+      ";
       $rs = mysqli_query($connection, $sql) or die("Error executing the query: " . mysqli_error($connection));
       
       while($data = mysqli_fetch_assoc($rs)) {
@@ -42,3 +57,21 @@
     <?php } ?>
   </tbody>
 </table>
+
+<br>
+<?php
+  $totalSQL = "SELECT id FROM 'contacts'";
+  $qr = mysqli_query($connection, $totalSQL) or die(mysqli_error($connection));
+  $numTotal = mysqli_num_rows($qr);
+  $pageTotal = ceil(($numTotal / $quantityOfContactsPerPage))
+
+  echo "total of contacts: $numTotal";
+
+  for($i = 1; $i <= $pageTotal; i++) {
+    if($i == $page) {
+      echo $i;
+    } else {
+      echo "<a href"\"page=$i\>$i</a>"
+    }
+  }
+?>
