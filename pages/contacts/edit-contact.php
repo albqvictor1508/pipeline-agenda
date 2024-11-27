90f5f2d4a706b934ef8456f6e6ad1674758809cd
Victor Arruda <albq.victor@gmail.com>
2024-11-27 14:11:36 -0300

    chore: photo removed

--- pages/contacts/edit-contact.php
+++ pages/contacts/edit-contact.php
@@ -6,8 +6,8 @@
 
 ?>
 
-<div class="row mt-5">
-  <div class="col-6">
+<div class=" mt-5">
+  <div class="">
     <h2>Edit Contact</h2>
     <div class="form-container">
       <div class="errors">
@@ -26,41 +26,9 @@
           <label for="exampleInputPhone1" class="form-label">Phone</label>
           <input type="tel" class="form-control" name="phone" value="<?= $data["phone"] ?>" id="exampleInputPhone1" aria-describedby="phoneHelp">
         </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" name="favority_flag" value="<?= $data["favority_flag"] ?>" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Favorite</label>
-      </div>
       <button type="submit" class="btn btn-primary" name="update-btn">Update Contact</button>
       </form>
     </div>
-  </div>
-  <div class="col-6">
-    <?php
-      if($data["photo"] == "" || !file_exists("assets/img/". $data['photo'])) {
-        $photoName = "no-photo.jpeg";
-      } else {
-        $photoName = $data["photo"];
-      }
-    ?>
-    <div class="mb-3"><img src="assets/img/<?= $photoName ?>" width="500px" alt=""></div>
-    <div class="mb-3">
-      <button class="edit-photo-btn">
-      <i class='bx bxs-camera'></i> Edit photo
-      </button>
-    </div>
-
-    <form action="" id="form-upload-photo" class="mb-3" method="post" encytype="multipart/form-data">
-      <input type="hidden" name="id" value="<?= $id ?>" />
-      <label class="form-label" for="fileInput">Select the photo</label>
-
-      <div class="input-group">
-        <input class="form-control" type="file" name="file" id="fileInput" />
-        <input type="submit" class="btn btn-secondary" value="Send">
-      </div>
-    </form>
-      <div id="preloader" class="mb-3 alert alert-success">
-        
-      </div>
   </div>
 </div>
 
